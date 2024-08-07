67746614aa583576ae3fa27b396326045549df63
Pragonna <alizadeemil5@gmail.com>
2024-08-07 16:09:40 +0400

    initial-

---
+++ Mediator.cs
@@ -0,0 +1,24 @@
+﻿using CustomMediatR.library.Abstractions;
+
+namespace CustomMediatR.library
+{
+    public class Mediator : IMediator
+    {
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
+        {
+            var requestType = request.GetType();
+            var requestInterface = requestType
+                                            .GetInterfaces()
+                                            .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequest<>))
+                                            .FirstOrDefault();
+
+            var responseType = requestInterface.GenericTypeArguments[0];
+
+            var genericType = typeof(IRequestHandler<,>).MakeGenericType(requestType, responseType);
+
+            var handler = MyServiceProvider.ServiceProvider.GetService(genericType);
+
+            return (Task<TResponse>)genericType.GetMethod("Handle").Invoke(handler, new object[] { request });
+        }
+    }
+}
